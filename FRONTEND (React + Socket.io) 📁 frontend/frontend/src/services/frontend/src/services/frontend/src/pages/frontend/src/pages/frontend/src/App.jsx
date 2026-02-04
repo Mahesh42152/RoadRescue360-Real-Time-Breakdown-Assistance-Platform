4ac9fe382952e340530dbeeb4ed1d03c03ea4f36
manohar <maheshbabu.ma2023@gmail.com>
2026-02-04 13:45:21 +0200

    Create App.jsx

---
+++ FRONTEND (React + Socket.io) 📁 frontend/frontend/src/services/frontend/src/services/frontend/src/pages/frontend/src/pages/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import BreakdownForm from "./pages/BreakdownForm";
+import LiveTracking from "./pages/LiveTracking";
+
+export default function App() {
+  return (
+    <div>
+      <h1>RoadRescue360</h1>
+      <BreakdownForm />
+      <LiveTracking />
+    </div>
+  );
+}
